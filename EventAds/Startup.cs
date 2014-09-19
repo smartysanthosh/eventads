00dd543d2ae2eb855866a8eee7c298739da382f1
srinivas rao pittla venkata <srinivasrao.pittlavenkata@gmail.com>
2014-09-19 16:31:07 +0530

    first check-in by srini and sandi

---
+++ EventAds/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventAds.Startup))]
+namespace EventAds
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
